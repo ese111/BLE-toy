20f5c0a3b617549d5cf5db8d706726df13e4c73b
ese111 <feung94@gmail.com>
2022-08-26 19:54:55 +0900

    build: 환경 세팅과 프로젝트 생성 언어 kotlin JDK:11 Sdk: min 28 compile 32 target 30 Gradle: 6.7

---
+++ app/src/main/java/com/example/bluetooth/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.bluetooth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bluetooth.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
